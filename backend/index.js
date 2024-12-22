a9f3d625ae49488ffb3dd78f7821f8f8a2cdb46c
Dnyanesh Sawant <sawantdnyanesh511@gmail.com>
2024-12-23 02:31:04 +0530

    mongo connected and user model created

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./src/routes/auth.route.js";
+
+import { connectDB } from "./src/lib/db.js";
 
 const app = express();
 
+dotenv.config();
+const PORT = process.env.PORT;
+
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-    console.log('server is running.');
+app.listen(PORT, () => {
+    console.log('server is running on port ' + PORT);
+    connectDB();
 });
