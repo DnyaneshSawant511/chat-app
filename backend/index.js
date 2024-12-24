f74712b473ad582753c5fb158e3030c9146627dd
Dnyanesh Sawant <sawantdnyanesh511@gmail.com>
2024-12-25 02:16:44 +0530

    message routes done

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./src/routes/auth.route.js";
 import cookieParser from "cookie-parser";
+
+import authRoutes from "./src/routes/auth.route.js";
+import messageRoutes from "./src/routes/message.route.js";
 
 import { connectDB } from "./src/lib/db.js";
 
@@ -14,6 +16,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log('server is running on port ' + PORT);
---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,91 @@
+import cloudinary from "../lib/cloudinary.js";
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
+
+//controller to get a list of all users except for the current logged in user
+export const getUsersForSideBar = async (req, res) => {
+
+    try {
+
+        const loggedInUserId = req.user._id;
+        const filteredUsers = await User.find({ _id: {$ne: loggedInUserId} }).select("-password");
+
+        res.status(200).json({
+            filteredUsers
+        });
+
+    } catch (error) {
+
+        console.log(error);
+        res.status(500).json({
+            error: "Internal Server Error"
+        });
+
+    }
+
+}
+
+//controller to get the chat history of the current logged in user with a particular user
+export const getMessages = async (req, res) => {
+
+    try {
+
+        const { id:otherUserId } = req.params; //id of other user passed in params
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            //either of the conditions satisfy
+            $or : [
+                {senderId: myId, receiverId: otherUserId},
+                {senderId: otherUserId, receiverId: myId}
+            ]
+        });
+
+        res.status(200).json({
+            messages
+        });
+
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            error: 'Internal Server Error'
+        });
+    }
+
+}
+
+export const sendMessage = async (req, res) => {
+
+    try {
+
+        const { text, image } = req.body;
+        const { id:receiverId } = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        });
+
+        await newMessage.save();
+
+        res.status(201).json({
+            newMessage
+        });
+
+    } catch(error){
+        console.log(error);
+        res.status(500).json({
+            error: "Internal Server Error"
+        });
+    }
+
+}
