55826729a41d019b296263f190db77d496faf82c
Dnyanesh Sawant <sawantdnyanesh511@gmail.com>
2024-12-27 14:14:53 +0530

    frontend routes and global states setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
+import Navbar from './components/Navbar';
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home.jsx';
+import Signup from './pages/Signup.jsx';
+import Login from './pages/Login.jsx';
+import Settings from './pages/Settings.jsx';
+import Profile from './pages/Profile.jsx';
+import { useAuthStore } from './store/useAuthStore.js';
 
 function App() {
 
+  const { authUser, checkAuth } =  useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log(authUser);
+
   return (
-    <>
-      <p className='text-gray-600'>hello</p>
-    </>
+    <div>
+
+      <Navbar />
+
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/profile" element={<Profile />} />
+
+      </Routes>
+
+    </div>
   )
 }
 
