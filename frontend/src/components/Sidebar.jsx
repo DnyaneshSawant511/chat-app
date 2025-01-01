0cb194a48c29b25ad1cc9909bdd0da75ffb9f33b
Dnyanesh Sawant <sawantdnyanesh511@gmail.com>
2025-01-02 00:33:43 +0530

    message frontend done

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -14,7 +14,6 @@ const Sidebar = () => {
     }, [getUsers]);
 
     if (isUsersLoading) return <SidebarSkeleton />;
-    console.log(users);
 
     return (
         
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
     messages: [],
     users: [],
     selectedUser: null,
@@ -23,7 +23,7 @@ export const useChatStore = create((set) => ({
         set({ isMessagesLoading: true });
         try {
             const res = await axiosInstance.get(`/messages/${userId}`);
-            set({ messages: res.data });
+            set({ messages: res.data.messages });
         } catch(error){
             toast.error(error.response.data.message);
         } finally {
@@ -32,5 +32,14 @@ export const useChatStore = create((set) => ({
     },
     setSelectedUser: (selectedUser) => {
         set({ selectedUser });
+    },
+    sendMessage: async (messageData) => {
+      const { selectedUser, messages } = get(); 
+      try {
+        const res = await axiosInstance.post(`messages/send/${selectedUser._id}`, messageData);
+        set({ messages: [...messages, res.data] });
+      } catch (error){
+        toast.error(error.response.data.message);
+      }
     },
 }));
